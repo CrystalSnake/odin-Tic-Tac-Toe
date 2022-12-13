0a3352dff97f35dcd10e9f06ecaeede191e2b0d6
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2022-12-13 22:24:13 +0400

    Detach createCell function

--- scripts.js
+++ scripts.js
@@ -1,25 +1,29 @@
 const Gameboard = (() => {
   let activePlayer = 1;
-  let gameboard = ['', '', 'X', '', '', 'O', 'X', '', ''];
+  let gameboard = ['', '', '', '', '', '', '', '', ''];
+  const createCell = (value) => {
+    const cell = document.createElement('div');
+    cell.classList.add('gameboard-cell');
+    cell.setAttribute('id', value);
+    cell.textContent = gameboard[value];
+    cell.addEventListener('click', () => {
+      if (activePlayer === 1 && gameboard[value] === '') {
+        cell.textContent = 'X';
+        gameboard[value] = 'X';
+        activePlayer = 2;
+      } else if (activePlayer === 2 && gameboard[value] === '') {
+        cell.textContent = 'O';
+        gameboard[value] = 'O';
+        activePlayer = 1;
+      }
+    });
+    return cell;
+  };
+
   const getGrid = () => {
     const container = document.querySelector('.container');
     for (let value in gameboard) {
-      const cell = document.createElement('div');
-      cell.classList.add('gameboard-cell');
-      cell.setAttribute('id', value);
-      cell.textContent = gameboard[value];
-      cell.addEventListener('click', () => {
-        if (activePlayer === 1 && gameboard[value] === '') {
-          cell.textContent = 'X';
-          gameboard[value] = 'X';
-          activePlayer = 2;
-        } else if (activePlayer === 2 && gameboard[value] === '') {
-          cell.textContent = 'O';
-          gameboard[value] = 'O';
-          activePlayer = 1;
-        }
-      });
-      container.appendChild(cell);
+      container.appendChild(createCell(value));
     }
   };
   return { getGrid };
