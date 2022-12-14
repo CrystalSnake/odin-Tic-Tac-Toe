bfeacb55710d7528841397a056a293ca7f0ee053
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2022-12-14 20:35:23 +0400

    Add game logic and player factory function

--- scripts.js
+++ scripts.js
@@ -1,32 +1,100 @@
+const container = document.querySelector('.container');
+let gameboard = ['', '', '', '', '', '', '', '', ''];
+let gameOn = true;
+
+const playerFactory = (name, mark) => {
+  let count = 0;
+  return { mark, name, count };
+};
+
+const player1 = playerFactory('one', 'X');
+const player2 = playerFactory('two', 'O');
+
+let activePlayer = player1;
+
+const getStartButton = () => {
+  const startNewGame = document.createElement('button');
+  startNewGame.textContent = 'New Game';
+  startNewGame.addEventListener('click', Gameboard.getGrid);
+  document.body.appendChild(startNewGame);
+};
+
 const Gameboard = (() => {
-  let activePlayer = 1;
-  let gameboard = ['', '', '', '', '', '', '', '', ''];
-  const createCell = (value) => {
+  const _createCell = (i) => {
     const cell = document.createElement('div');
     cell.classList.add('gameboard-cell');
-    cell.setAttribute('id', value);
-    cell.textContent = gameboard[value];
+    cell.textContent = gameboard[i];
     cell.addEventListener('click', () => {
-      if (activePlayer === 1 && gameboard[value] === '') {
-        cell.textContent = 'X';
-        gameboard[value] = 'X';
-        activePlayer = 2;
-      } else if (activePlayer === 2 && gameboard[value] === '') {
-        cell.textContent = 'O';
-        gameboard[value] = 'O';
-        activePlayer = 1;
+      if (gameboard[i] === '' && gameOn) {
+        cell.textContent = activePlayer.mark;
+        gameboard[i] = activePlayer.mark;
+        GameLogic.winCheck();
+        GameLogic.changePlayer();
       }
     });
     return cell;
   };
 
   const getGrid = () => {
-    const container = document.querySelector('.container');
-    for (let value in gameboard) {
-      container.appendChild(createCell(value));
+    gameboard = ['', '', '', '', '', '', '', '', ''];
+    container.textContent = '';
+    for (let i in gameboard) {
+      container.appendChild(_createCell(i));
     }
+    gameOn = true;
   };
+
   return { getGrid };
 })();
 
+const GameLogic = (() => {
+  const winCheck = () => {
+    if (
+      (gameboard[0] === activePlayer.mark &&
+        gameboard[1] === activePlayer.mark &&
+        gameboard[2] === activePlayer.mark) ||
+      (gameboard[3] === activePlayer.mark &&
+        gameboard[4] === activePlayer.mark &&
+        gameboard[5] === activePlayer.mark) ||
+      (gameboard[6] === activePlayer.mark &&
+        gameboard[7] === activePlayer.mark &&
+        gameboard[8] === activePlayer.mark) ||
+      (gameboard[0] === activePlayer.mark &&
+        gameboard[3] === activePlayer.mark &&
+        gameboard[6] === activePlayer.mark) ||
+      (gameboard[1] === activePlayer.mark &&
+        gameboard[4] === activePlayer.mark &&
+        gameboard[7] === activePlayer.mark) ||
+      (gameboard[2] === activePlayer.mark &&
+        gameboard[5] === activePlayer.mark &&
+        gameboard[8] === activePlayer.mark) ||
+      (gameboard[0] === activePlayer.mark &&
+        gameboard[4] === activePlayer.mark &&
+        gameboard[8] === activePlayer.mark) ||
+      (gameboard[2] === activePlayer.mark &&
+        gameboard[4] === activePlayer.mark &&
+        gameboard[6] === activePlayer.mark)
+    ) {
+      gameOn = false;
+      activePlayer.count++;
+      console.log(`${activePlayer.name} Win! Count ${activePlayer.count}`);
+    } else {
+      let emptyCells = gameboard.filter((cell) => cell != 'X' && cell != 'O');
+      if (emptyCells.length === 0) {
+        console.log('Draw!');
+        gameOn = false;
+      }
+    }
+  };
+  const changePlayer = () => {
+    if (activePlayer === player1) {
+      activePlayer = player2;
+    } else {
+      activePlayer = player1;
+    }
+  };
+  return { winCheck, changePlayer };
+})();
+
 Gameboard.getGrid();
+getStartButton();
